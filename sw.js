32922be5ade50592a840d0f967f853fb5f53abe1
leolikestocode <leonardofpereira25@gmail.com>
2019-07-04 07:08:34 -0300

    Fixing service worker bugs

--- sw.js
+++ sw.js
@@ -1,36 +1,48 @@
-this.addEventListener('install', function(event){
+let staticCacheVersion = 'rastaurant-cache1';
+
+let urlToCache = [
+	'./',
+	'./index.html',
+	'./restaurant.html',
+	'./css/styles.css',
+	'./data/restaurants.json',
+	'./js/dbhelper.js',
+	'./js/main.js',
+	'./js/restaurant_info.js',
+	'./js/sw_register.js',
+	'./img/1.jpg',
+	'./img/2.jpg',
+	'./img/3.jpg',
+	'./img/4.jpg',
+	'./img/5.jpg',
+	'./img/6.jpg',
+	'./img/7.jpg',
+	'./img/8.jpg',
+	'./img/9.jpg',
+	'./img/10.jpg'
+];
+
+self.addEventListener('install', function(event){
 	event.waitUntil(
-		caches.open('restaurantMap').then(function(cache){
-			return cache.addAll([
-				'/',
-				'index.html',
-				'restaurant.html',
-				'css/styles.css',
-				'js/dbhelper.js',
-				'js/main.js',
-				'js/restaurant_info.js',
-				'img/1.jpg',
-				'img/2.jpg',
-				'img/3.jpg',
-				'img/4.jpg',
-				'img/5.jpg',
-				'img/6.jpg',
-				'img/7.jpg',
-				'img/8.jpg',
-				'img/9.jpg',
-				'img/10.jpg'
-			]);
-		})
+		caches
+			.open(staticCacheVersion)
+			.then(function(cache){
+				console.log('cache installed:', cache);
+				return cache.addAll(urlToCache);
+			})
+			.catch((err) => {
+				console.log('err:', err);
+			})
 	);
 });
 
-this.addEventListener('activate', function(event){
+self.addEventListener('activate', function(event){
 	event.waitUntil(
 		caches.keys().then(function(cacheNames){
 			return Promise.all(
 				cacheNames
 					.filter(function(cacheName){
-						return cacheName === 'restaurantMap';
+						return cacheName.startsWith('restaurant-') && cacheName != staticCacheVersion;
 					})
 					.map(function(cacheName){
 						return caches.delete(cacheName);
@@ -39,7 +51,6 @@ this.addEventListener('activate', function(event){
 		})
 	);
 });
-
 this.addEventListener('fetch', (event) => {
 	event.respondWith(
 		caches.match(event.request, { ignoreSearch: true }).then(function(response){
